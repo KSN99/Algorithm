0344e3264624fedd771c232c6008f0a9e3da97db
KSN <v999vvv@naver.com>
2022-10-03 01:18:28 +0900

    stack

--- stack.py
+++ stack.py
@@ -1,16 +1,13 @@
 class Stack:
     def __init__(self):
-        self.items= [] #데이터 저장ㅇ을 위한 리스트
-    
+        self.items= [] #데이터 저장을 위한 리스트
     def push(self , val):
         self.items.append(val)
-    
     def pop(self):
         try:
             return self.items.pop()
         except IndexError:          #pop할 아이템이 없으면 indexerror 발생. 
-            print("Stack is empty")
-    
+            print("Stack is empty")    
     def top(self):
         try:
             return self.items[-1]
@@ -18,3 +15,10 @@ class Stack:
             print("Stack is empty")
     def __len__(self):          #len()로 호출하면 stack의 item수 반환 
         return len(self.items)
+
+S=Stack()
+S.push(10) #리스트에 10 추가.
+S.push(2)
+print(S.pop()) #2
+print(S.top()) #10
+print(len(S)) #1
